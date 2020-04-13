eeac1cce8ebb5770cbe129eaec917608737c5c39
Kevin Chinchilla <kchinchillam98@gmail.com>
2020-04-12 21:09:06 -0600

    probando quasar framework

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar App Camera
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+      :width="250"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Menu
+        </q-item-label>
+        <q-separator />
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink'
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: 'Camara',
+          icon: 'photo_camera'
+        },
+        {
+          title: 'Fotos',
+          icon: 'crop_original'
+        },
+        {
+          title: 'Ayuda',
+          icon: 'help',
+          link: '/help'
+        }
+      ]
+    }
+  }
+}
+</script>
